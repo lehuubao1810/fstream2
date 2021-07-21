03cae960cca95078137941340f0e909c09decf12
lehuubao1810 <79296917+lehuubao1810@users.noreply.github.com>
2021-07-21 19:11:11 +0700

    Revert "updateMain.cpp"

--- main.cpp
+++ main.cpp
@@ -2,38 +2,38 @@
 #include<fstream>
 
 using namespace std;
-fstream file;
+
 struct game {
     char name[20];
     int id ;
 };
 
-void ghi_File(game x)
+void ghi_File(std::fstream file,game &x)
 {
     file.open("QLGAME.DAT",ios::app);
     file<<x.name<<"\n"<<x.id<<endl;
     file.close();
 }
 
-
-void xuat_File(game x)
+void doc_File(fstream file,game x)
 {
     file.open("QLGAME.DAT",ios::in);
     file>>x.name>>x.id;
-    while(!file.eof())
-    {
+}
+void xuat_File(fstream file,game x)
+{
+    while(!file.eof()) {
         cout<<"Name: "<<x.name<<endl;
         cout<<"ID: "<<x.id<<endl;
-        file>>x.name>>x.id;
+        file.close();
     }
-    file.close();
 }
 
 int main() {
+    fstream file;
     game x;
     cout<<"Nhap ten :";cin>> x.name;
     cout<<"Nhap id :";cin>> x.id;
-    ghi_File(x);
-    xuat_File(x);
+    ghi_File(file,x);
     return 0;
 }
