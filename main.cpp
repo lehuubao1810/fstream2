fd61a52e7021f33ca57b3587d5a126280ec4e120
lehuubao1810 <79296917+lehuubao1810@users.noreply.github.com>
2021-07-21 18:40:53 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+struct game {
+    char name[20];
+    int id ;
+};
+
+void ghi_File(std::fstream file,game &x)
+{
+    file.open("QLGAME.DAT",ios::app);
+    file<<x.name<<"\n"<<x.id<<endl;
+    file.close();
+}
+
+void doc_File(fstream file,game x)
+{
+    file.open("QLGAME.DAT",ios::in);
+    file>>x.name>>x.id;
+}
+void xuat_File(fstream file,game x)
+{
+    while(!file.eof()) {
+        cout<<"Name: "<<x.name<<endl;
+        cout<<"ID: "<<x.id<<endl;
+        file.close();
+    }
+}
+
+int main() {
+    fstream file;
+    game x;
+    cout<<"Nhap ten :";cin>> x.name;
+    cout<<"Nhap id :";cin>> x.id;
+    ghi_File(file,x);
+    return 0;
+}
